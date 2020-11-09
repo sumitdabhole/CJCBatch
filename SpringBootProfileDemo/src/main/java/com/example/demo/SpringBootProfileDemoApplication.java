b2555ba1bd574dae5a3ef17a36102b3c3923eef7
India <India@DESKTOP-3F5VKRB>
2020-11-09 16:11:23 +0530

    commit1

---
+++ SpringBootProfileDemo/src/main/java/com/example/demo/SpringBootProfileDemoApplication.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProfileDemoApplication {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("In Spring Boot Profile Concept demo");
+		SpringApplication.run(SpringBootProfileDemoApplication.class, args);
+	}
+
+}
